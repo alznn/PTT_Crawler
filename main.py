c12dd0cfd0c7f8adcff8b4b11912352ee4d466d1
alznn <zxcv19960410@gmail.com>
2018-04-17 14:17:45 +0800

    處理 error 404 與其他非常建發文格式

--- main.py
+++ main.py
@@ -2,45 +2,51 @@ import requests
 import re
 import lxml
 from bs4 import BeautifulSoup
+import pandas as pd
 reg = "</div>.*--"
 #<\/div>\n(.*[\S\s]+)\-\-
-def output(author,title,date,content,table):
-    print(author,"\n",title,"\n",date,"\n",content,"\n",table)
+def output(tmp):
+    data = pd.Series(tmp)
+    data.columns = ["author", "title", "date", "content"]
+    data.to_csv("Movie.csv",mode = 'a', encoding='utf-8', index=False,header=True)
+
 
 def InfoAndContent(links):
-    contenUrl = "https://www.ptt.cc/bbs/movie/"
-    i = 0
-    user = []
-    context = []
+    outFile = []
     for link in links:
-#        news_url = "https://www.ptt.cc/" + link
-        print(links[0])
         tmp = []
-        res = requests.get(links[0])
+        news_url = "https://www.ptt.cc/" + link
+#        news_url = "https://www.ptt.cc/bbs/movie/M.1092719966.A.C44.html"
+        print(news_url)
+        res = requests.get(news_url)
         soup = BeautifulSoup(res.text.encode('utf-8'), 'lxml')
         main_content = soup.find(id="main-content")
-        metas = main_content.select('div.article-metaline')
-        author = metas[0].select('span.article-meta-value')[0].getText()
-        title = metas[1].select('span.article-meta-value')[0].getText()
-        date = metas[2].select('span.article-meta-value')[0].getText()
-        tmp.append(author)
-        tmp.append(title)
-        tmp.append(date)
-        filtered = [v for v in main_content.stripped_strings if v[0] not in [u'※', u'◆'] and v[:2] and not soup.select('div.class.push')]
-        ##filtered = [_f for _f in filtered if _f]
-        content = ' '.join(filtered)
-#        content = re.sub(r'(\s)+', '', content)
+        if str(main_content) == "None":
+            print("Hi")
+        else:
+            metas = main_content.select('div.article-metaline')
+            author = metas[0].select('span.article-meta-value')[0].getText()
+            title = metas[1].select('span.article-meta-value')[0].getText()
+            date = metas[2].select('span.article-meta-value')[0].getText()
+            tmp.append(author)
+            tmp.append(title)
+            tmp.append(date)
+            filtered = [v for v in main_content.stripped_strings if v[0] not in [u'※', u'◆'] and v[:2] and not soup.select('div.class.push')]
+            content = ' '.join(filtered)
 
-        content = re.sub(r'(\-\-)+.*', '', content)
-        content = re.sub(r'作者.+\n','', content)
-        content = re.sub(r'\n','', content)
-        #tmp.append(content)
-        #tmp.append("movie")
-#        print(author)
-#        print(title)
-#        print(date)
-        print("content:\n",content)
-        #output(author,title,date,content,"movie")
+            content = re.sub(r'(\-\-)+.*', '', content)
+            content = re.sub(r'[作者|發信人].+([[1|2]([0-9]{3}))','', content)
+            #content = re.sub(r'.+轉信站.+\n', '', content)
+            content = re.sub(r'\n','', content)
+            tmp.append(content)
+            print(author)
+            print(title)
+            print(date)
+            #print("content:\n",content)
+            #outFile.append(tmp)
+            output(tmp)
+
+
 # main
 links = []
 
@@ -50,7 +56,7 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 articles = soup.find_all('div', 'r-ent')
 #movie 版 index : 1-6583,""
-for i in range(2,3):
+for i in range(1,3):
     if i == 0:
         url = 'https://www.ptt.cc/bbs/movie/index.html'  #newest
     else:
@@ -66,7 +72,7 @@ for i in range(2,3):
         title = meta.getText().strip(" ")
  #       print(meta)
         link = meta.get('href')
- #       print(link)
-        links.append("https://www.ptt.cc/bbs/movie/M.1089252949.A.35A.html")
+#        print(link)
+        links.append(link)
 
     InfoAndContent(links)
